c9660e2c5be2d4669d48b2a43180435eb03915c3
Michael Totaro <mstotaro@gmail.com>
2025-01-25 23:16:55 -0500

    Fixed css reload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pathlib
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from sqlmodel import SQLModel
 from database.db import engine
 from strawberry.fastapi import GraphQLRouter
@@ -34,3 +35,5 @@ app.include_router(graphql_app, prefix='/graphql')
 async def serve_html():
     html_file = pathlib.Path('welcome/landing_page.html')
     return HTMLResponse(content=html_file.read_text())
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
