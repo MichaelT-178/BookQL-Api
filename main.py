cc9a18f092e3fa6ce652f27f0de27c9504ba8873
Michael Totaro <mstotaro@gmail.com>
2025-01-26 00:38:19 -0500

    landing

--- main.py
+++ main.py
@@ -35,5 +35,3 @@ app.include_router(graphql_app, prefix='/graphql')
 async def serve_html():
     html_file = pathlib.Path('welcome/landing_page.html')
     return HTMLResponse(content=html_file.read_text())
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
